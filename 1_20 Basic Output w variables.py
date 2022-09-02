e96da30b30fb69a0d149113d059ff73bf7345706
ksourign <111767114+ksourign@users.noreply.github.com>
2022-09-01 22:21:23 -0500

    Add files via upload

---
+++ 1_20 Basic Output w variables.py
@@ -0,0 +1,18 @@
+#Kaitlyn Sourignosack
+#1824497
+
+a = int (input("Enter integer:" "\n"))
+
+
+print ("You entered:", a)
+squared = a*a
+print (a, "squared is", squared)
+cubed = a*a*a
+print ("And",a, "cubed is", cubed, "!!")
+b = int (input("Enter another integer:""\n"))
+
+
+addition = a+b
+multiplication = a*b
+print(a, "+", b ,"is", addition)
+print(a ,"*", b, "is", multiplication)
