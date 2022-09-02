ca283f6648d4f4d25f1dc5c0af92c30e5a0c69fd
ksourign <111767114+ksourign@users.noreply.github.com>
2022-09-01 22:14:20 -0500

    Update 1_12 Basics.py

--- 1_12 Basics.py
+++ 1_12 Basics.py
@@ -1,4 +1,7 @@
+#Kaitlyn Sourignosack
+#1824497
+
 userNum = int (input())
-userNumSquared = userNum * userNum   # Bug here; fix it when instructed
+userNumSquared = userNum * userNum   
    
-print(userNumSquared)       # Output formatting issue here; fix it when instructed
+print(userNumSquared)       
