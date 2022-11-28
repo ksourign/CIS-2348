0e9d0540ba4f26286474d359e0f4870c60839929
ksourign <111767114+ksourign@users.noreply.github.com>
2022-11-28 06:28:21 -0600

    Update FinalProjectPastServiceDateInventory.py

--- FinalProjectPart2/FinalProjectPastServiceDateInventory.py
+++ FinalProjectPart2/FinalProjectPastServiceDateInventory.py
@@ -16,7 +16,6 @@ with open (r"ServiceDatesList.csv", 'r') as csv_servicedateslist:
     for i in contentlist:
         datelist.append (i[1])
         contentlist_copy.append (i)
-    # print ('datelist',datelist)
 
 
     for j in datelist:
@@ -24,15 +23,10 @@ with open (r"ServiceDatesList.csv", 'r') as csv_servicedateslist:
         dateobject_list.append (dateobject)
 
     dateobject_list.sort ()
-    # print ('dateobject list',dateobject_list)
 
     for i in  range (len (dateobject_list)):
         dateformat = dateobject_list[i].strftime("%m/%d/%Y")
         dateobject_list [i] = dateformat
-    # print ('dateobjectlist',dateobject_list)
-
-    #sortted dates w details
-    # print ('copy content', contentlist_copy)
 
 
 #Sorting dates from least to greatest with correct ID
@@ -44,19 +38,5 @@ with open (r"ServiceDatesList.csv", 'r') as csv_servicedateslist:
                 listsorteddate_wdetails.append (i)
             else:
                 continue
-    # print ('list sorted dates details',listsorteddate_wdetails)
-
-
-    # for i in listsorteddate_wdetails:
-    #     print(','.join(i))
-
-
-
-
-
-
-
-
-
 
 
